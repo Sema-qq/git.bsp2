f921f5c522b98728ac85d7663ef1b50932f2a6be
Sema-qq <PO-2-06_024@mail.ru>
2017-07-22 16:44:45 +0500

    add project

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="ru">
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>Заголовок</title>
+	<meta name="description" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/countdown/jquery.countdown.css" />
+	<link rel="stylesheet" href="css/fonts.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<link rel="stylesheet" href="css/media.css" />
+</head>
+<body>
+    <header class="top_header">
+        <div class="header_topline">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <button class="auth_buttons hidden-md hidden-lg hidden-sm"><i class="fa fa-user"></i></button>
+                        <div class="top_links">
+                            <a href="#">Вход</a> /
+                            <a href="#">Регистрация</a>
+                        </div>
+                        <div class="social_buttons">
+                            <a href="#"><i class="fa fa-vk"></i></a>
+                            <a href="#"><i class="fa fa-github"></i></a>
+                            <a href="#"><i class="fa fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#" class="logo">moDesign</a>
+                    <nav class="main_menu clearfix">
+                        <button class="main_menu_button hidden-md hidden-lg"><i class="fa fa-bars"></i></button>
+                        <ul>
+                            <li class="active"><a href="#">Главная</a></li>
+                            <li><a href="#">Приложения</a></li>
+                            <li><a href="#">Портфолио</a></li>
+                            <li><a href="#">Отзывы</a></li>
+                        </ul>
+                        <div class="top_contacts"><i class="fa fa-mobile"></i> 8-800-700-1000</div>
+                    </nav>
+                    <h1>Центр разработки мобильных приложений</h1>
+                    <div class="slider_container">
+                        <div class="next_button"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>
+                        <div class="prev_button"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                        <div class="carousel">
+                            <div class="slide_item"><img src="img/1.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/2.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/3.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/4.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/5.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/6.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/11.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/13.jpg" alt=""></div>
+                            <div class="slide_item"><img src="img/14.jpg" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="order">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="sell_description">Самые лучшие приложения</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="sell_right">
+                        <a href="#callback" class="button fancybox">Заказать разработку</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="main_content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="content_text">
+                        <div class="blog_item">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3><a href="#">Заголовок статьи</a></h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="img/1.jpg">
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog_item">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3><a href="#">Заголовок статьи</a></h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="img/1.jpg">
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog_item">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3><a href="#">Заголовок статьи</a></h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="img/1.jpg">
+                                </div>
+                                <div class="col-md-8">
+                                    <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <aside class="right_aside">
+                        <ul>
+                            <li><a href="#">Что такое Lorem Ipsum?</a></li>
+                            <li><a href="#">Почему он используется?</a></li>
+                            <li><a href="#">Откуда он появился?</a></li>
+                            <li><a href="#">Где его взять?</a></li>
+                            <li><a href="#">Что такое Lorem Ipsum?</a></li>
+                            <li><a href="#">Почему он используется?</a></li>
+                            <li><a href="#">Откуда он появился?</a></li>
+                            <li><a href="#">Где его взять?</a></li>
+
+                        </ul>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="hidden">
+        <form id="callback" class="pop_form">
+            <h3>Заказать обратный звонок</h3>
+            <input type="text" name="name" placeholder="Ваше имя..." required/>
+            <input type="text" name="phone" placeholder="Телефон" required/>
+            <button type="submit" class="button">Заказать</button>
+        </form>
+    </div>
+	<!--[if lt IE 9]>
+	<script src="libs/html5shiv/es5-shim.min.js"></script>
+	<script src="libs/html5shiv/html5shiv.min.js"></script>
+	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="libs/respond/respond.min.js"></script>
+	<![endif]-->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/waypoints/waypoints-1.6.2.min.js"></script>
+	<script src="libs/scrollto/jquery.scrollTo.min.js"></script>
+	<script src="libs/owl-carousel/owl.carousel.min.js"></script>
+	<script src="libs/countdown/jquery.plugin.js"></script>
+	<script src="libs/countdown/jquery.countdown.min.js"></script>
+	<script src="libs/countdown/jquery.countdown-ru.js"></script>
+	<script src="libs/landing-nav/navigation.js"></script>
+	<script src="js/common.js"></script>
+	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
+</body>
+</html>
